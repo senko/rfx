4052edef56e50b115ea3521e508d94105dfd847d
Senko Rasic <senko.rasic@collabora.co.uk>
2010-01-06 02:19:38 +0100

    lang/{en,hr}.php: set up en as the main language and hr as the translation

--- lang/en.php
+++ lang/en.php
@@ -1,17 +1,9 @@
 <?php
 
-$TRANSLATION_TABLE = array(
-    'Logiraj se' => 'Log in',
-    'Lozinka' => 'Password',
-    'Registriraj se' => 'Register',
-    'Više informacija' => 'Find out more',
-);
-
 function __($txt)
 {
-    global $TRANSLATION_TABLE;
-    if (isset($TRANSLATION_TABLE[$txt])) return $TRANSLATION_TABLE[$txt];
     return $txt;
 }
+
 
 ?>
--- lang/hr.php
+++ lang/hr.php
@@ -1,7 +1,16 @@
 <?php
 
+$TRANSLATION_TABLE = array(
+    'Log in' => 'Logiraj se',
+    'Password' => 'Lozinka',
+    'Register' => 'Registriraj se',
+    'Find out more' => 'Više informacija',
+);
+
 function __($txt)
 {
+    global $TRANSLATION_TABLE;
+    if (isset($TRANSLATION_TABLE[$txt])) return $TRANSLATION_TABLE[$txt];
     return $txt;
 }
 
